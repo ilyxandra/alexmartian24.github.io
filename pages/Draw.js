318020a3774d3690e7f77d0ee7b4b0ed6dbdd42b
Sachin Iyer <sachinjiyer@gmail.com>
2022-08-23 03:29:20 -0700

    abstract setup to general function

--- pages/Draw.js
+++ pages/Draw.js
@@ -2,20 +2,26 @@ import dynamic from 'next/dynamic'
 const Sketch = dynamic(() => import('react-p5').then((mod) => mod.default), { ssr: false })
 
 function Draw() {
-  const setup = (p5, canvasParentRef) => {
-    p5.createCanvas(window.innerWidth, window.innerHeight).parent(canvasParentRef)
+  const genSetup = p5 => {
     p5.background(10, 130, 10)
   }
+
+  const setup = (p5, canvasParentRef) => {
+    p5.createCanvas(window.innerWidth, window.innerHeight).parent(canvasParentRef)
+    genSetup(p5)
+  }
+
+  const resize = p5 => {
+    p5.resizeCanvas(window.innerWidth, window.innerHeight)
+    genSetup(p5)
+  }
+
 
   const draw = p5 => {
     var width = window.innerWidth
     var height = window.innerHeight
     p5.ellipse(width / 2, height / 2, 200)
     p5.ellipse(width / 1, height / 3, 100)
-  }
-
-  const resize = p5 => {
-    p5.resizeCanvas(window.innerWidth, window.innerHeight)
   }
 
 
