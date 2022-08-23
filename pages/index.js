7dad4f2a3f07e30f92a7226f195b93120bad0509
Alex <alexmartian24@gmail.com>
2022-08-23 01:02:55 -0400

    commit message

---
+++ pages/index.js
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>"i'm so unhappy"</title>
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+         Focketh yew m8
+        </h1>
+
+
+        <div className={styles.grid}>
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+
+      </footer>
+    </div>
+  )
+}
