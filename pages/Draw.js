e24e59866073fedafd1665cabb0e7d2c32d0508b
Sachin Iyer <sachinjiyer@gmail.com>
2022-08-23 03:25:43 -0700

    use the react-p5 native window resizing tools to increase rendering speed

--- pages/Draw.js
+++ pages/Draw.js
@@ -4,32 +4,22 @@ const Sketch = dynamic(() => import('react-p5').then((mod) => mod.default), { ss
 function Draw() {
   const setup = (p5, canvasParentRef) => {
     p5.createCanvas(window.innerWidth, window.innerHeight).parent(canvasParentRef)
-
-    function handleResize() {
-      p5.resizeCanvas(window.innerWidth, window.innerHeight)
-    }
-
-    window.addEventListener("resize", handleResize);
-
-  }
-
-  const renderdraw = (p5, width, height) => {
-    p5.background(255, 130, 20)
-    p5.ellipse(width / 20, height / 20, 200)
-    p5.ellipse(width / 10, height / 30, 100)
-    p5.square(300, 10)
+    p5.background(10, 130, 10)
   }
 
   const draw = p5 => {
-    renderdraw(p5, window.innerWidth, window.innerHeight)
-    function handleResize() {
-      renderdraw(p5, window.innerWidth, window.innerHeight)
-    }
-    window.addEventListener("resize", handleResize);
+    var width = window.innerWidth
+    var height = window.innerHeight
+    p5.ellipse(width / 2, height / 2, 200)
+    p5.ellipse(width / 1, height / 3, 100)
+  }
+
+  const resize = p5 => {
+    p5.resizeCanvas(window.innerWidth, window.innerHeight)
   }
 
 
-  return <Sketch setup={setup} draw={draw} />
+  return <Sketch setup={setup} draw={draw} windowResized={resize} />
 }
 
 export default Draw
