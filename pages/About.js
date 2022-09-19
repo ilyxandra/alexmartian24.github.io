7fa8ed78767fecb6444a5ebe46852111bd9c69d9
Alex <alexmartian24@gmail.com>
2022-09-19 14:27:56 -0400

    i forgot what i changed...

--- pages/About.js
+++ pages/About.js
@@ -1,8 +1,13 @@
 export default function Home() {
     return (
+        <style>
+
+        }
+        </style>
         <div>
-            <p>here</p>
+            <h1>About Me</h1>
+            <p id = "info"></p>
         </div>
+
     )
   }
-  
