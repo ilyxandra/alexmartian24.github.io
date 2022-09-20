251298979b934b85af5665367e3296ffd326386a
Sachin Iyer <sachinjiyer@gmail.com>
2022-09-19 21:46:29 -0400

    fix about page compilation issues

--- pages/About.js
+++ pages/About.js
@@ -1,13 +1,9 @@
 export default function Home() {
     return (
-        <style>
-
-        }
-        </style>
         <div>
             <h1>About Me</h1>
-            <p id = "info"></p>
+            <p id="info"></p>
         </div>
 
     )
-  }
+}
