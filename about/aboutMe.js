60ff96f65ba1fa0c3a984a193274edbe7889a511
Alex Martin <alexmartian24@gmail.com>
2024-02-04 16:48:48 -0500

    pls work

--- about/aboutMe.js
+++ about/aboutMe.js
@@ -6,9 +6,10 @@ var groundX = 0;
 var groundY = height - 80;
 let treeArr = [];
 let gravity = 9.81;
+let w = 0.1;
 
 function setup() {
-  tree = loadImage("/images/WinterTree.jpg");
+  tree = loadImage("/images/WinterTree.png");
   createCanvas(width,height);
 }
 
@@ -27,7 +28,7 @@ class Snowflake{
   this.color = "snow";
   this.size = random(0,5);
   this.angle = random(10, 1*PI);
-  this.radius = sqrt(random(pow(width / 1, 2)));
+  this.radius = sqrt(random(pow(100 / 1, 2)));
   }
   cleanup(){
     for (i = 0; i < snowflakeArr.length(); i++){
@@ -35,7 +36,11 @@ class Snowflake{
         splice(i, 1);
     }
   }
-  fall(){}
+  fall(w){
+    let angle = w * time + this.angle;
+    this.posX = 100 / 1 + this.radius * tan(angle); //calculates tangent of the angle the petals fall
+    this.posY += pow(this.size, 0.5);
+  }
 }
 
 
